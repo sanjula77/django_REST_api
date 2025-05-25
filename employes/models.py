3893b06289331c487f910c0c0bc378d572de8490
Gihan sanjula <sanjulagihan94@gmail.com>
2025-05-26 00:29:18 +0530

    Add Employe model and register in admin; create initial migration

--- employes/models.py
+++ employes/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Employe(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    position = models.CharField(max_length=100)
-    hire_date = models.DateField()
+    emp_id = models.CharField(max_length=20, unique=True)
+    emp_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.emp_name} {self.emp_id}"
